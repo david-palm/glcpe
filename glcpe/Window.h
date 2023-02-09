342b1da93867cc5b0d3ca2e80d1c6f3aaa16535f
david-palm <david.palm1996@gmail.com>
2023-02-09 02:55:41 +0100

    Add Events

--- glcpe/Window.h
+++ glcpe/Window.h
@@ -1,6 +1,6 @@
 #pragma once
 
-//#include "Events/Event.h"
+#include "Events/Event.h"
 
 #include <string>
 
@@ -10,7 +10,7 @@ struct WindowProperties
     unsigned int width;
     unsigned int height;
 
-    WindowProperties(const std::string& title = "GLWE", unsigned int width = 1280, unsigned int height = 720)
+    WindowProperties(const std::string& title = "GLCPE", unsigned int width = 1280, unsigned int height = 720)
             : title(title), width(width), height(height)
     {}
 };
@@ -18,7 +18,7 @@ struct WindowProperties
 class Window
 {
 public:
-    //using EventCallbackFunction = std::function<void(Event&)>;
+    using EventCallbackFunction = std::function<void(Event&)>;
 
     virtual ~Window() {}
 
@@ -26,7 +26,7 @@ public:
     virtual unsigned int getWidth() const = 0;
     virtual unsigned int getHeight() const = 0;
 
-    //virtual void setEventCallback(const EventCallbackFunction& callback) = 0;
+    virtual void setEventCallback(const EventCallbackFunction& callback) = 0;
     virtual void setVSync(bool enabled) = 0;
     virtual bool isVSync() const = 0;
     static Window* create(const WindowProperties& properties = WindowProperties());
