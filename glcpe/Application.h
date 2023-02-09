0903adfb0de782a79ef33386176ae5bf7cd577c2
david-palm <david.palm1996@gmail.com>
2023-02-09 08:45:19 +0100

    Add functions to Application class

--- glcpe/Application.h
+++ glcpe/Application.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <iostream>
-
 #include "glad/glad.h"
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
@@ -10,9 +8,22 @@
 #include <SDL_opengles2.h>
 #else
 #include <SDL2/sdl.h>
+#endif
+
+#include <iostream>
 #include <memory>
 
-#endif
+#include "Renderer/Shader.h"
+#include "Renderer/Buffer.h"
+#include "Renderer/VertexArray.h"
+
+#include "Window.h"
+#include "Layer.h"
+#include "LayerStack.h"
+
+#include "Events/WindowEvent.h"
+#include "Events/KeyEvent.h"
+#include "Events/MouseEvent.h"
 
 class Application
 {
@@ -24,28 +35,28 @@ public:
     //Emscripten limitation: MainLoop needs to be separate function
     void runLoop();
 
-    //void onEvent(Event& event);
+    void onEvent(Event& event);
 private:
     void run();
 #ifndef __EMSCRIPTEN__
-    //bool onWindowClose(WindowCloseEvent& event);
-    //bool onWindowResize(WindowResizeEvent& event);
+    bool onWindowClose(WindowCloseEvent& event);
+    bool onWindowResize(WindowResizeEvent& event);
 #endif
-    //bool onKeyDown(KeyDownEvent& event);
-    //bool onKeyUp(KeyUpEvent& event);
+    bool onKeyDown(KeyDownEvent& event);
+    bool onKeyUp(KeyUpEvent& event);
 
-    //bool onMouseDown(MouseDownEvent& event);
-    //bool onMouseUp(MouseUpEvent& event);
-    //bool onMouseMove(MouseMoveEvent& event);
+    bool onMouseDown(MouseDownEvent& event);
+    bool onMouseUp(MouseUpEvent& event);
+    bool onMouseMove(MouseMoveEvent& event);
 
-    //void pushLayer(Layer* layer);
-    //void pushOverlay(Layer* layer);
+    void pushLayer(Layer* layer);
+    void pushOverlay(Layer* layer);
 private:
     static Application* s_Instance;
     bool m_Running = false;
     SDL_Window* m_Window;
     //std::unique_ptr<Window> m_Window;
-    //LayerStack m_LayerStack;
+    LayerStack m_LayerStack;
 };
 
 
